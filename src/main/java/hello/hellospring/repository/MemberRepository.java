12c221b02893dd07718102bf664d4cdc3331235a
jjindol <andy15948@naver.com>
2023-07-11 01:06:34 +0900

    230711 commit

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package hello.hellospring.repository;
+import hello.hellospring.domain.Member;
+import java.util.List;
+import java.util.Optional;
+public interface MemberRepository {
+    Member save(Member member); // 회원 저장
+    Optional<Member> findById(Long id); // ID로 회원 찾기
+    Optional<Member> findByName(String name); // 이름으로 찾기
+    List<Member> findAll(); // 모든 회원 list 반환
+}
