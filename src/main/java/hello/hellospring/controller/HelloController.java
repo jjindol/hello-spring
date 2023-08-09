b98d9802b42a0a9f44c17f47de0bff72a7accc35
jjindol <andy15948@naver.com>
2023-08-09 18:34:18 +0900

    230809 commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,6 +1,10 @@
 package hello.hellospring.controller;
 
+
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,17 +14,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @GetMapping("hello-mvc") // 'hello-mvc' 경로로 GET 요청이 들어오면 메소드 실행
-    public String helloMvc(@RequestParam("name") String name, Model model) {
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-template"; // "hello-template" 뷰를 렌더링
+        return "hello-template"; // hello-template 뷰를 렌더링
     }
 
 
     @GetMapping("hello-string")
-    @ResponseBody
+    @ResponseBody // body 영역에 내용을 그대로 넣어줌
     public String helloString(@RequestParam("name") String name) {
-            return "hello " + name; // "hello spring"
+        return "hello " + name;
     }
+
 
     @GetMapping("hello-api") // 객체를 넘기면 JsonConverter 동작
     @ResponseBody // 응답 결과를 HTTP 응답 본문에 직접 작성하도록 지정
@@ -30,14 +35,9 @@ public class HelloController {
         return hello;
     }
 
+    @Getter
+    @Setter
     static class Hello {
         private String name;
-
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 }
