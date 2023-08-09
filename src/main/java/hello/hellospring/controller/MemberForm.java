7cfce1ec0c6639c954064197eff4bdfd3f49b9ec
jjindol <andy15948@naver.com>
2023-08-10 02:35:59 +0900

    230810 commit

--- src/main/java/hello/hellospring/controller/MemberForm.java
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -1,15 +1,11 @@
 package hello.hellospring.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MemberForm { // 멤버 등록 시 사용되는 폼 데이터를 담는데 사용
 
     private String name; // private 으로 선언되어 외부 접근 x
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
